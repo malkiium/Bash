f1ac4a13527e6b780f5f22dfb6a5a97dcd22c157
malkiium <elihalldelafosse@gmail.com>
2025-10-08 21:09:15 +0200

    Translate prompts and output messages in notes.sh to English for consistency

--- notes.sh
+++ notes.sh
@@ -6,7 +6,7 @@ moyenne=0
 
 while [ $nots != "#" ]
 do
-    read -p "Veuillez entrer vos notes : " nots
+    read -p "please enter your grades : " nots
     if [ $nots != "#" ]
     then
         let total=$total+$nots
@@ -14,4 +14,4 @@ do
     fi
 done
 let moyenne=$total/$numbs
-echo -e "votre moyenne est : $moyenne"
+echo -e "your average is : $moyenne"
