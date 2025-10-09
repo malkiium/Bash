133ffd8fe05ec16b913c0689ca042b9fba0e80ec
malkiium <elihalldelafosse@gmail.com>
2025-10-09 08:44:24 +0200

    Refactor etudiiant.sh to improve variable initialization and maintain French prompts for consistency

--- etudiiant.sh
+++ etudiiant.sh
@@ -2,21 +2,31 @@
 avrg=0
 stdname=" "
 res=" "
+stdcount=0
+totalgrds=0
 
 while true
 do
-    read -p "what is the student name ? : " stdname
+    read -p "quelle est l'étudiant ? : " stdname
 
     if [ $stdname = "#" ]
     then
         break
     fi
+    let stdcount=$stdcount+1
 
-    read -p "enter the 3 grades : " grd1 grd2 grd3
+    read -p "entrer les 3 notes : " grd1 grd2 grd3
     let grds=$grd1+$grd2+$grd3
     let avrg=$grds/3
+    let totalgrds=$totalgrds+$avrg
 
 
-    res="$res\n$stdname 's average is : $avrg"
+    res="$res\n$stdname 's moyenne est de : $avrg"
 done
+
+let totalgrds=$totalgrds/$stdcount
+
+echo -e ""
+echo -e "voici les Résultats :"
 echo -e "$res"
+echo -e "la moyenne est de : $totalgrds"
