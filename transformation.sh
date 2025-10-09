21d97236d9cb6a4097846918291dd1f103702fb1
malkiium <elihalldelafosse@gmail.com>
2025-10-09 09:22:36 +0200

    Refactor transformation.sh to improve file handling and user prompts

--- transformation.sh
+++ transformation.sh
@@ -1,10 +1,35 @@
-read -p "donner un fichier : " x
-if [ -f $x ]
-then
-    echo -e "que vouller vous faire ? \n 1. transformer en maj \n 2. transformer en min \n 3 crypter \n"
-    read usrinp
-    if [ $usrinp -eq 1 ]
+#!/bin/bash
+trial="yes"
+
+while [ trial != "no" ]
+do
+    read -p "donner un fichier : " x
+    if [ -f $x ]
     then
-        tr "a-z" "A-Z" $x
+        echo -e "que vouller vous faire ? \n 1. transformer en maj \n 2. transformer en min \n 3 crypter \n"
+        read usrinp
+        if [ $usrinp -eq 1 ]
+        then
+            tr "a-z" "A-Z" < $x
+        fi
+        if [ $usrinp -eq 2 ]
+        then
+            tr "A-Z" "a-z" < $x
+        fi
+        if [ $usrinp -eq 3 ]
+        then
+            read -p "quelles sont les options d'encriptages ? : " opt1 opt2
+            echo -e " "
+            tr "$opt1" "$opt2" < $x
+        fi
+        echo -e ""
+        break
+    else
+        echo "ce n'est pas un fichier."
+        read -p "vouller vous recommencer ? " trial
+        if [ $trial = "no" ]
+        then
+            break
+        fi
     fi
-fi
+done
