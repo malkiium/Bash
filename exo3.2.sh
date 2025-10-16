1682e25d928492141f127169d23186fbc6d0cde7
malkiium <elihalldelafosse@gmail.com>
2025-10-16 09:05:50 +0200

    Add exo5.2.sh script for line counting and phrase input

--- exo3.2.sh
+++ exo3.2.sh
@@ -5,24 +5,31 @@ count=0
 
 while [ $count -lt 3 ]
 do
-    read -p "code ? : " code
+    read -p "code ? : " -t 5 code
 
     let count=$count+1
-
-    for i in $l
-    do
-
-        if [ $i = $code ]
-        then
-            echo -e "code kept "
-            exit
-        fi
-    done
+    if [ -z $code ]
+    then
+        echo "vide. veuillez réessayer."
+    else
+        for i in $l
+        do
+            if [ $i = $code ]
+            then
+                echo -e "\033[32m code kept "
+                echo -e "\033[0m"
+                exit
+            fi
+        done
+        echo -e "\033[31m code éronner"
+        echo -e "\033[0m"
+    fi
 done
 
 let count=$count+1
 
 if [ $count -ge 4 ]
 then
-    echo -e "trop de tentatives"
+    echo -e "\033[31m trop de tentatives"
+    echo -e "\033[0m"
 fi
---
+++ exo5.2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+nmbdlng=0
+
+while true
+do
+    read -p "entrer vos phrases : " entr
+    if [ "$entr" = "#" ]
+    then
+        echo -e "nombre de lignes : $nmbdlng"
+        tr -s '\n' < entrertxt
+        rm entrertxt
+        exit
+    else
+        echo "$entr" >> entrertxt
+        let nmbdlng=$nmbdlng+1
+    fi
+done
+
+
